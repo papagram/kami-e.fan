baaa0bd768d5366118908382a6148443e75cd7b7
SHINYA MIYATA <kamiefan@gmail.com>
2015-01-26 10:45:27 +0900

    .gitignore作成　db_config_sample.php作成

---
+++ config/db_config_sample.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_server = 'localhost';
+$db_user = 'user_name';
+$db_password = 'your_password';
+$db_name = 'kamie';
+$db_port = '1234';
+
+$dsn = "mysql:host={$db_server};port={$db_port};dbname={$db_name};charset=utf8";
