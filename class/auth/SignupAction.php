22fe56f2cea9c4e820fb558c996bbad085a10e19
SHINYA MIYATA <kamiefan@gmail.com>
2015-02-16 11:23:57 +0900

    SignupActionクラス　executeメソッド　POST値のバリデートを追加

--- class/auth/SignupAction.php
+++ class/auth/SignupAction.php
@@ -32,15 +32,42 @@ class SignupAction
 			$_SESSION['signup']['input_name'] = '';
 			$_SESSION['signup']['input_email'] = '';
 			
-			// ▼ バリデートを考える
+			// ▼ 入力値チェック　Name
+			$posts['name'] = mb_ereg_replace('\A(\s)+|(\s)+\z', '', $posts['name']); // 前後のスペースは削除
+			$name_max_len = 30;
+			$len = mb_strlen($posts['name']); // 文字数取得
+			if (! $len) {
+				throw new InvalidValueException('名前を入力して下さい！');
+			}
 			
+			if ($len > $name_max_len) {
+				throw new InvalidValueException('名前は{$name_max_len}文字までです。');
+			}
 			
-			$model = new UsersModel($this->dsn, $this->db_user, $this->db_password);
+			// ▼ 入力値チェック　Email
+			$posts['email'] = mb_ereg_replace('\A(\s)+|(\s)+\z', '', $posts['email']); // 前後のスペースは削除
+			$email_max_len = 50;
+			$len = mb_strlen($posts['email']); // 文字数取得
+			if (! filter_var($posts['email'], FILTER_VALIDATE_EMAIL) || ! $len)
+			{
+				throw new InvalidValueException('メールアドレスを入力して下さい！');
+			}
+			
+			if ($len > $email_max_len) {
+				throw new InvalidValueException('メールアドレスが長すぎます。');
+			}
 			
 			// ▼ POSTで渡されたEmailが登録済みか確認
+			$model = new UsersModel($this->dsn, $this->db_user, $this->db_password);
 			$res = $model->exsistEmail($posts['email']);
 			if (! $res) {
 				throw new AlreadyExistsException('このメールアドレスはすでに登録されています。');
+			}
+			
+			// ▼ 入力値チェック　Password
+			$posts['password'] = mb_ereg_replace('\A(\s)+|(\s)+\z', '', $posts['password']); // 前後のスペースは削除
+			if (! is_match_pattern_password($posts['password'])) {
+				throw new InvalidValueException('もう一度パスワードを設定して下さい。');
 			}
 			
 			// ▼ POSTで渡されたパスワードをハッシュ化
@@ -50,10 +77,7 @@ class SignupAction
 			
 			// ▼ ユーザーを登録
 			$model->registUser($posts['name'], $posts['email'], $hash_password, $regist_flg, $activation_key);
-			
-			// ▼ メール送信・・・はやめるかも
-			
-			
+
 			// ▼ ログイン画面へリダイレクト
 			redirect('/auth/login_index.php?');
 		} else {
